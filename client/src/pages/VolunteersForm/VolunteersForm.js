c1fa218d2742fa26df8fab56d14ac467adca8f44
JakariaJishan <jakariajishan987@gmail.com>
2022-04-13 21:46:59 +0600

    form functionality done

--- client/src/pages/VolunteersForm/VolunteersForm.js
+++ client/src/pages/VolunteersForm/VolunteersForm.js
@@ -1,24 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 
 const VolunteersForm = () => {
   const param = useParams();
- const displayName = sessionStorage.getItem('displayName');
- const email = sessionStorage.getItem('email');
+  const displayName = sessionStorage.getItem("displayName");
+  const email = sessionStorage.getItem("email");
+  const [startDate, setStartDate] = useState(new Date());
 
- const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+   const newData = {
+      ...data, startDate
+    };
+    console.log(newData);
+    fetch("http://localhost:5000/api/user/info", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newData),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("Name", { required: true, maxLength: 20 })} placeholder='User Name' />
-      <input {...register("email", { pattern: /^[A-Za-z]+$/i })} placeholder='Email' />
-      <input {...register("description", { required: true,  })} placeholder='description'/>
-      <input {...register("org", { required: true,  })} placeholder='org' />
+    <div className="grid place-items-center bg-slate-500 ">
+      <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 ">
+        <input
+          {...register("Name", { required: true, maxLength: 20 })}
+          placeholder="User Name"
+          value={displayName}
+        />
+        <input {...register("email")} placeholder="Email" value={email} />
+        <input
+          {...register("description", { required: true })}
+          placeholder="description"
+        />
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
 
-      <button type="submit">submit</button> 
-    </form>
+        <input
+          {...register("org", { required: true })}
+          placeholder="org"
+          value={param.id}
+        />
+
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,20 +1,45 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose');
-const port = 5000
-require('dotenv').config()
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
+const port = 5000;
+const cors = require("cors");
+const admin = require("firebase-admin");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+require("dotenv").config();
+
+
+const uri = process.env.DB_STRING;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 //user authentication
 
-const userRoute = require('./user/userRoute');
-app.use('/api/user/', userRoute)
+const userRoute = require("./user/userRoute");
+app.use("/api/user/", userRoute);
 
-mongoose.connect(process.env.DB_STRING, (req, res) => {
-  console.log('db connect')
+client.connect((err) => {
+  const charityCollection = client.db('charify').collection('charify-collection');
+  app.post('/api/user/info', (req, res) => {
+    const newCollection = req.body;
+    // charityCollection.insertOne(newCollection).then(result => {
+    //   console.log(result)
+    // })
+    console.log(newCollection)
+  })
 })
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
